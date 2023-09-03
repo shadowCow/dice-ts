99fdd1b3e30fe0eca95caad96f24a299dfe37096
shadowCow <shadowCow84@gmail.com>
2023-09-03 12:17:13 -0400

    roll up to 7 d6

--- src/dice.ts
+++ src/dice.ts
@@ -8,6 +8,41 @@ export function rollD6x2(): [D6Value, D6Value] {
   return [rollD6(), rollD6()];
 }
 
+export function rollD6x3(): [D6Value, D6Value, D6Value] {
+  return [rollD6(), rollD6(), rollD6()];
+}
+
+export function rollD6x4(): [D6Value, D6Value, D6Value, D6Value] {
+  return [rollD6(), rollD6(), rollD6(), rollD6()];
+}
+
+export function rollD6x5(): [D6Value, D6Value, D6Value, D6Value, D6Value] {
+  return [rollD6(), rollD6(), rollD6(), rollD6(), rollD6()];
+}
+
+export function rollD6x6(): [
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+] {
+  return [rollD6(), rollD6(), rollD6(), rollD6(), rollD6(), rollD6()];
+}
+
+export function rollD6x7(): [
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+  D6Value,
+] {
+  return [rollD6(), rollD6(), rollD6(), rollD6(), rollD6(), rollD6(), rollD6()];
+}
+
 export function rollDie(sides: number): number {
   return Math.floor(Math.random() * sides) + 1;
 }
